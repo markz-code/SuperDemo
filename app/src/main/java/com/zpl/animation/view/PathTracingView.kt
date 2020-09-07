b7c58a3efffad227b678ed145cca916e31b04ad4
zpl <zplalipay@gmail.com>
2020-09-07 21:59:24 +0800

    实现路径动画

--- app/src/main/java/com/zpl/animation/view/PathTracingView.kt
+++ app/src/main/java/com/zpl/animation/view/PathTracingView.kt
@@ -9,15 +9,18 @@ import android.graphics.PathMeasure
 import android.util.AttributeSet
 import android.view.View
 import android.view.animation.LinearInterpolator
-import java.util.jar.Attributes
 
 /**
- * 物体随着圆转圈
+ * 路径动画（getSegment）
  */
-class PathPosTanView @JvmOverloads constructor(context: Context, attributes: AttributeSet? = null) :
+class PathTracingView @JvmOverloads constructor(
+    context: Context,
+    attributes: AttributeSet? = null
+) :
     View(context, attributes),
     View.OnClickListener {
 
+    private val mDst = Path()
     private val mPath: Path = Path()
     private val mPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.STROKE
@@ -28,41 +31,39 @@ class PathPosTanView @JvmOverloads constructor(context: Context, attributes: Att
     private val mTan = FloatArray(2)
     private val mAnimator: ValueAnimator
     private var mCurrentValue: Float = 0.0f
+    private var mLength: Float
 
     init {
-        mPath.addCircle(0f, 0f, 200f, Path.Direction.CW)
-        mPathMeasure.setPath(mPath, false)
+        mPath.addCircle(400f, 400f, 100f, Path.Direction.CW)
+        mPathMeasure.setPath(mPath, true)
+        mLength = mPathMeasure.length
         setOnClickListener(this)
         mAnimator = ValueAnimator.ofFloat(0f, 1f).apply {
-            duration = 3000
+            duration = 1000
             interpolator = LinearInterpolator()
             repeatCount = ValueAnimator.INFINITE
             addUpdateListener {
                 mCurrentValue = it.animatedValue as Float
                 invalidate()
             }
+            start()
         }
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
-        mPathMeasure.getPosTan(mCurrentValue * mPathMeasure.length, mPos, mTan)
-        // 求出当前点的切线角度
-        var degree = (Math.atan2(mTan[1].toDouble(), mTan[0].toDouble()) * 180 / Math.PI).toFloat()
+        mDst.reset()
+        // 此处不加好像有bug
+        mDst.lineTo(0f, 0f)
+        var stop = mLength * mCurrentValue
+        var start = stop - (0.5f - Math.abs(mCurrentValue - 0.5)) * mLength
 
-        canvas!!.apply {
-            save()
-            translate(400f, 400f)
-            drawPath(mPath, mPaint)
-            drawCircle(mPos[0], mPos[1], 10f, mPaint)
-            rotate(degree)
-            drawLine(0f, -200f, 300f, -200f, mPaint)
-            restore()
-        }
+        mPathMeasure.getSegment(start.toFloat(), stop, mDst, true)
+        canvas!!.drawPath(mDst, mPaint)
     }
 
     override fun onClick(p0: View?) {
-        mAnimator.start()
+
     }
 }
