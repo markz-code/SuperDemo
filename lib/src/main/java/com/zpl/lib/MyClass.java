8870a70a788477b3860355dab29ac69a7a984ba0
zpl <zplalipay@gmail.com>
2021-02-16 18:11:45 +0800

    生产者消费者模式

--- lib/src/main/java/com/zpl/lib/MyClass.java
+++ lib/src/main/java/com/zpl/lib/MyClass.java
@@ -3,11 +3,78 @@ package com.zpl.lib;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 实现生产者和消费者模式
+ */
 public class MyClass {
-    public static void main(String[] args) {
-        List<?> lists = new ArrayList<>();
 
+    private static Integer count = 0;
+    // 缓存池大小
+    private static final Integer FULL = 10;
+    private static final String LOCK = "lock";
+
+    public static void main(String[] args) {
+        MyClass myClass = new MyClass();
+       new Thread(myClass.new Producer()).start();
+       new Thread(myClass.new Producer()).start();
+       new Thread(myClass.new Producer()).start();
+       new Thread(myClass.new Producer()).start();
+       new Thread(myClass.new Consumer()).start();
+       new Thread(myClass.new Consumer()).start();
+       new Thread(myClass.new Consumer()).start();
+       new Thread(myClass.new Consumer()).start();
+    }
+
+    class Producer implements Runnable {
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (LOCK) {
+                    while (count.equals(FULL)) {
+                        try {
+                            LOCK.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    count++;
+                    System.out.println(Thread.currentThread().getName() + "生产者生产，目前总共有" + count);
+                    LOCK.notify();
+                }
+            }
+        }
     }
 
 
+    class Consumer implements Runnable {
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (LOCK) {
+                    while (count == 0) {
+                        try {
+                            LOCK.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    count--;
+                    System.out.println(Thread.currentThread().getName() + "消费者消费，目前总共有" + count);
+                    LOCK.notifyAll();
+                }
+            }
+        }
+    }
 }
